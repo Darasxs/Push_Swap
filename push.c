24baf1d65fc31e6152bb1f4502d3fc66742544f4
Dariusz Paluszkiewicz <dpaluszk@1-C-4.42heilbronn.de>
2024-05-29 18:16:14 +0200

    wrote function for the pa operation

---
+++ push.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 15:09:50 by dpaluszk          #+#    #+#             */
+/*   Updated: 2024/05/29 18:15:58 by dpaluszk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pa(t_list **stack_a, t_list **stack_b, int check)
+{
+	t_list	*tmp;
+
+	if(check == 1)
+		write(1, "pa", 3);
+	if(stack_a != NULL && stack_b != NULL)
+	{
+		if(*stack_b)
+		{
+			tmp = *stack_b;
+			*stack_b = (*stack_b)->next;
+			tmp->next = *stack_a;
+			*stack_a = tmp;
+		}
+	}
+}
--- swap.c
+++ swap.c
@@ -6,20 +6,20 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:09:16 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/05/29 15:02:19 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/05/29 17:21:53 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	*sa(t_list **stack, int check)
+void	sa(t_list **stack, int check)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
 
 	if (check == 1)
 		write(1, "sa", 3);
-	if (stack != 0)
+	if (stack != NULL)
 	{
 		if (*stack && (*stack)->next)
 		{
@@ -30,17 +30,16 @@ void	*sa(t_list **stack, int check)
 			*stack = tmp2;
 		}
 	}
-	return (*stack);
 }
 
-void	*sb(t_list **stack, int check)
+void	sb(t_list **stack, int check)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
 
 	if (check == 1)
 		write(1, "sb", 3);
-	if (stack != 0)
+	if (stack != NULL)
 	{
 		if (*stack && (*stack)->next)
 		{
@@ -51,12 +50,14 @@ void	*sb(t_list **stack, int check)
 			*stack = tmp2;
 		}
 	}
-	return (*stack);
 }
 
 void	ss(t_list **stack_a, t_list **stack_b)
 {
-	sa(stack_a, 0);
-	sb(stack_b, 0);
-	write(1, "ss", 3);
+	if (stack_a != NULL && stack_b != NULL)
+	{
+		sa(stack_a, 0);
+		sb(stack_b, 0);
+		write(1, "ss", 3);
+	}
 }
