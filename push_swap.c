b54ee13edf587ef2406b8894fb80443a1b21a5ee
Dariusz Paluszkiewicz <dpaluszk@2-E-7.42heilbronn.de>
2024-07-07 13:22:51 +0200

    updated match_nodes function

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 13:44:09 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/07/06 16:55:30 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/07/07 13:22:37 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	match_nodes(t_list *stack_a, t_list *stack_b)
 		tmp = stack_a;
 		while (tmp)
 		{
-			if (stack_b->content < stack_a->content && (target == NULL
+			if (stack_b->content < tmp->content && (target == NULL
 					|| tmp->content > target->content))
 				target = tmp;
 			tmp = tmp->next;
@@ -42,32 +42,3 @@ void	push_swap(t_list **stack_a, t_list **stack_b)
 		pa(stack_a, stack_b);
 	sort_three(stack_a);
 }
-
-
-//////////////////////////////////////////////////
-//void match_nodes(t_list *stack_a, t_list *stack_b)
-//{
-//	t_list	*current_a;
-//	t_list	*target_node;
-//	long	best_match_index;
-	
-//	while(stack_b)
-//	{
-//		best_match_index = LONG_MAX;
-//		current_a = stack_a;
-//		while(current_a)
-//		{
-//			if(current_a->content > stack_b->content && current_a->content < best_match_index)
-//			{
-//				best_match_index = current_a->content;
-//				target_node = current_a;
-//			}
-//			current_a = current_a->next;
-//		}
-//		if(best_match_index == LONG_MAX)
-//			stack_b->target = find_smallest_number(stack_a);
-//		else
-//			stack_b->target = target_node;
-//		stack_b = stack_b->next;
-//	}
-//}  
